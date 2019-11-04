682529b7ced6c84d7024893d130ea67af77f8b45
Abi Vickery <vickeryabi@gmail.com>
2019-11-04 11:14:39 -0600

    ph()

---
+++ app.py
@@ -0,0 +1,35 @@
+def ph(): # return the category given the ph level
+    print("What is the ph level?")
+    lvl =int(input())
+    if lvl == 0:
+        print("Strong acid")
+    elif lvl == 1:
+        print("Strong acid")
+    elif lvl == 2:
+        print("Strong acid")
+    elif lvl == 3:
+        print("Strong acid")
+    elif lvl == 4:
+        print("Strong acid")
+    elif lvl == 5:
+        print("Weak acid")
+    elif lvl == 6:
+        print("Weak acid")
+    elif lvl == 7:
+        print("Neutral")
+    elif lvl == 8:
+        print("Weak base")
+    elif lvl == 9:
+        print("Weak base")
+    elif lvl == 10:
+        print("Strong base")
+    elif lvl == 11:
+        print("Strong base")
+    elif lvl == 12:
+        print("Strong base")
+    elif lvl == 13:
+        print("Strong base")
+    elif lvl == 14:
+        print("Strong base")
+
+ph()
