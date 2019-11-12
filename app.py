2a1b436f710c20f6fcbbe8c495672dbbefacaa56
Abi Vickery <vickeryabi@gmail.com>
2019-11-12 16:35:28 -0600

    other factors
    adding in other factors!
    (Still need to finish the rest of the alphabet!)

--- app.py
+++ app.py
@@ -35,6 +35,56 @@ def ph(): # return the category given the ph level
         print("Error, number is too high!")
     elif lvl < 0:
         print("Error, number is too low (can not do negative numbers.)")
+    elif lvl == '!':
+        print("Error, not a number!")
+    elif lvl == '@':
+        print("Error, not a number!")
+    elif lvl == '#':
+        print("Error, not a number!")
+    elif lvl == '$':
+        print("Error, not a number!")
+    elif lvl == '%':
+        print("Error, not a number!")
+    elif lvl == '^':
+        print("Error, not a number!")
+    elif lvl == '&':
+        print("Error, not a number!")
+    elif lvl == '*':
+        print("Error, not a number!")
+    elif lvl == '(':
+        print("Error, not a number!")
+    elif lvl == ')':
+        print("Error, not a number!")
+    elif lvl == '-':
+        print("Error, not a number!")
+    elif lvl == '_':
+        print("Error, not a number!")
+    elif lvl == '=':
+        print("Error, not a number!")
+    elif lvl == '+':
+        print("Error, not a number!")
+    elif lvl == ' ':
+        print("That's not a number or a character, thats just a space!")
+    elif lvl == 'A', 'a':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'B', 'b':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'C', 'c':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'D', 'd':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'E', 'e':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'F', 'f':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'G', 'g':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'H', 'h':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'I', 'i':
+        print("Error, that's a letter, not a number!")
+    elif lvl == 'J', 'j':
+        print("Error, that's a letter, not a number!")
 
 ph()
 
